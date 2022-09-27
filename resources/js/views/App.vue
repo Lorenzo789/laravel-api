ac9496dbdea89f914c9f9d1884015902d7ac76ac
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-27 17:38:52 +0200

    index posts done to do user

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,12 +1,46 @@
 <template>
   <div>
-    <h1>Work in progress</h1>
+    <div class="card p-4 my-4" v-for="post in posts" :key="post.id">
+      <div class="card-title my-2">
+        <h3>{{ post.id }} - {{ post.title }}</h3>
+      </div>
+      <div class="card-subtitle my-2">
+        <h6>autore</h6>
+      </div>
+      <div class="card-text my-2">
+        <p>{{ post.post_content }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
-    name:'App',
+  name: 'App',
+  data: function(){
+    return {
+      posts: [],
+    }
+  },
+  methods: {
+    getPosts() {
+      axios.get('/api/posts', {
+
+      }).then((response) => {
+        // console.log(response.data);
+        this.posts = response.data.data;
+        console.log(this.posts);
+      }).catch((error) => {
+        console.warn(error);
+      })
+    }
+  },
+  created(){
+    this.getPosts();
+  }
 }
 </script>
 
