5ac9ec91d29a3e088a93308bb23ff90d98ffebf8
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-28 15:28:24 +0200

    add link to api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,12 +87,9 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="http://127.0.0.1:8000/api/tags/">Tags Api Index</a>
+                    <a href="http://127.0.0.1:8000/api/posts/">Posts Api Index</a>
                     <a href="http://127.0.0.1:8000/kokokokoko">Index</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
