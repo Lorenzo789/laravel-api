7d053e6b4b7fea1a3d1fb1b268636062efedc3e9
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-28 14:28:05 +0200

    create components and props

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,49 +1,42 @@
 <template>
   <div>
-    <div class="card my-4 p-3 mx-auto" style="max-width: 800px;" v-for="post in posts" :key="post.id">
-      <div class="card-title my-2">
-        <h3>{{ post.id }} - {{ post.title }}</h3>
-      </div>
-      <div class="card-image text-center my-3">
-        <img :src="post.post_image" class="w-50" alt="">
-      </div>
-      <div class="card-subtitle my-2">
-        <h6>{{ post.user.name }} - {{ post.post_date }}</h6>
-      </div>
-      <div class="card-text my-2">
-        <p>{{ post.post_content }}</p>
-      </div>
-    </div>
+    <h1 class="text-center my-4">Recent Posts:</h1>
+    <Main v-for="post in posts" :key="post.id"
+      :post="post"
+    />
   </div>
 </template>
 
 <script>
 
 import axios from 'axios';
+import Main from '../components/Main.vue';
 
 export default {
-  name: 'App',
-  data: function(){
-    return {
-      posts: [],
-    }
-  },
-  methods: {
-    getPosts() {
-      axios.get('/api/posts', {
-
-      }).then((response) => {
-        // console.log(response.data);
-        this.posts = response.data.data;
-        console.log(this.posts);
-      }).catch((error) => {
-        console.warn(error);
-      })
-    }
-  },
-  created(){
-    this.getPosts();
-  }
+    name: "App",
+    components: {
+      Main,
+    },
+    data: function () {
+        return {
+            posts: [],
+        };
+    },
+    methods: {
+        getPosts() {
+            axios.get("/api/posts", {}).then((response) => {
+                // console.log(response.data);
+                this.posts = response.data.data;
+                console.log(this.posts);
+            }).catch((error) => {
+                console.warn(error);
+            });
+        }
+    },
+    created() {
+        this.getPosts();
+    },
+    components: { Main }
 }
 </script>
 
